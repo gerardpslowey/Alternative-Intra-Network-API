3348c13613dac261e4d653c2e9c3346791cf9270
William OHanlon <william.ohanlon2@mail.dcu.ie>
2020-06-24 20:44:20 +0100

    Update conftest.py to working version

--- conftest.py
+++ conftest.py
@@ -1,18 +1,18 @@
 import pytest
-
 from api import create_app
 
-global address
+# Give each test the same app to test
+# This allows the actually Firebase database to be tested
+app_context =  create_app()[0]
 
-global API
+# These fixtures are ran before tests and set up the environment for tests e.g. variables 
 
-app_context = create_app()[0]
-
+# API the tests can call (tests call the name of the function as the variable returned by the function)
 @pytest.fixture
 def app():
     yield app_context
 
-
+# Tests use this client to make requets instead of starting the server
 @pytest.fixture
 def client(app):
     return app.test_client()
